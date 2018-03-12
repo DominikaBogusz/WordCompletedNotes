334cf07e15ce973306d1914128b5a4ed387eda3b
DominikaBogusz <dominika.bogu.bogusz@gmail.com>
2018-03-12 22:16:55 +0100

    Added textBox and listing completions in listBox

---
+++ WordCompletedNotes/MainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WordCompletedNotes
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(13, 13);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(273, 236);
+            this.textBox.TabIndex = 0;
+            this.textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(292, 13);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(130, 238);
+            this.listBox.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 261);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.textBox);
+            this.Name = "MainForm";
+            this.Text = "WordCompletedNotes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
---
+++ WordCompletedNotes/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WordCompletion;
+
+namespace WordCompletedNotes
+{
+    public partial class MainForm : Form
+    {
+        private IComplementarable dictionary;
+        string nextWord = "";
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            dictionary = new SimpleCompletion();
+        }
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)32 || e.KeyChar == (char)13)
+            {
+                if (nextWord != "")
+                {
+                    dictionary.Insert(nextWord);
+                    nextWord = "";
+                }
+            }
+            else if (e.KeyChar == (char)8)
+            {
+                if (nextWord != "")
+                {
+                    nextWord = nextWord.Remove(nextWord.Length - 1);
+                }
+            }
+            else
+            {
+                nextWord += e.KeyChar;
+            }
+
+            listBox.Items.Clear();
+            if (nextWord != "")
+            {
+                List<string> a = dictionary.FindMostUsedMatches(nextWord);
+                listBox.Items.AddRange(a.ToArray());
+            }
+        }
+    }
+}
